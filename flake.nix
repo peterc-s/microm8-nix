acc0824a3a55d42d7f90212b1f8ba1c625bd4d2c
peterc-s <petercs@purelymail.com>
2026-03-05 17:21:04 +0000

    Allow unfree

--- flake.nix
+++ flake.nix
@@ -12,7 +12,10 @@
     flake-utils,
   }:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
       runtimeLibs = [
         pkgs.portaudio
